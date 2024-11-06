6edb58ed7544356300e62f7f827229303716f036
Shady Khalifa <dev+github@shadykhalifa.me>
2024-11-06 17:29:40 +0200

    chore: update the code to latest SDK

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
+use color_eyre::eyre;
 use color_eyre::Result;
 use frost_blueprint as blueprint;
 use gadget_sdk::{
     self as sdk,
-    runners::{tangle::TangleConfig, BlueprintRunner},
+    runners::{tangle::TangleConfig, BlueprintConfig, BlueprintRunner},
 };
-use sdk::tangle_subxt::*;
 use tracing::Instrument;
 
 #[sdk::main(env)]
+#[tracing::instrument(skip(env), name = "frost-blueprint", parent = env.span.clone(), err)]
 async fn main() -> Result<()> {
-    let span = env.span.clone();
-    let _spanned = span.enter();
+    color_eyre::install()?;
+
     let signer = env.first_sr25519_signer()?;
     let network_identity = {
         let ed25519 = env.first_ed25519_signer()?.signer().clone();
         sdk::libp2p::identity::Keypair::ed25519_from_bytes(ed25519.seed())?
     };
     let my_ecdsa_key = env.first_ecdsa_signer()?;
-    let client = subxt::OnlineClient::from_url(&env.ws_rpc_endpoint).await?;
+    let client = env.client().await?;
     let network_config = sdk::network::setup::NetworkConfig::new_service_network(
         network_identity,
         my_ecdsa_key.signer().clone(),
@@ -26,21 +27,29 @@ async fn main() -> Result<()> {
         env.target_port,
         blueprint::NETWORK_PROTOCOL,
     );
-    let gossip_handle = sdk::network::setup::start_p2p_network(network_config)?;
-    let tangle = env.protocol_specific.tangle()?;
-
-    let service_id = match tangle.service_id {
-        Some(service_id) => service_id,
-        None => {
-            sdk::error!("Service ID not found, exiting...");
-            return Ok(());
-        }
-    };
+    let gossip_handle = sdk::network::setup::start_p2p_network(network_config)
+        .map_err(|e| eyre::eyre!("Failed to start the network: {}", e))?;
+    let tangle = env
+        .protocol_specific
+        .tangle()
+        .map_err(|e| eyre::eyre!("Failed to get tangle configuration: {}", e))?;
+    let config = TangleConfig::default();
 
     // Create your service context
     // Here you can pass any configuration or context that your service needs.
     let context = blueprint::ServiceContext::new(env.clone(), gossip_handle)?;
 
+    let service_id = match tangle.service_id {
+        Some(service_id) => service_id,
+        None if config.requires_registration(&env).await? => {
+            sdk::info!("Running in registration mode, so no service ID is required.");
+            0
+        }
+        None => {
+            sdk::error!("Service ID not found, exiting...");
+            return Ok(());
+        }
+    };
     // Create the event handler from the job
     let keygen = blueprint::keygen::KeygenEventHandler {
         service_id,
@@ -57,11 +66,11 @@ async fn main() -> Result<()> {
     };
 
     sdk::info!("Starting the event watcher ...");
-    BlueprintRunner::new(TangleConfig::default(), env)
+    BlueprintRunner::new(config, env)
         .job(keygen)
         .job(sign)
         .run()
-        .instrument(span.clone())
+        .in_current_span()
         .await?;
     sdk::info!("Exiting...");
     Ok(())
