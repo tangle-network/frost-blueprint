59a617c4c526a0f175b847c0cb304d10a166eb1d
Drew Stone <drewstone329@gmail.com>
2025-02-21 16:45:14 -0700

    chore: update to 0.3.0

--- contracts/src/FrostBlueprint.sol
+++ contracts/src/FrostBlueprint.sol
@@ -9,5 +9,12 @@ import "tnt-core/BlueprintServiceManagerBase.sol";
  * `BlueprintServiceManagerBase` contract, which provides the basic functionality
  */
 contract FrostBlueprint is BlueprintServiceManagerBase {
-
+    function operatorAddressFromPublicKey(bytes memory operatorPublicKey) public pure returns (address) {
+        // First hash with keccak256 and then with ripemd160
+        // Convert the ripemd160 hash to address by taking the last 20 bytes
+        bytes32 keccakHash = keccak256(operatorPublicKey);
+        bytes memory encoded = abi.encodePacked(keccakHash);
+        bytes20 ripemdHash = ripemd160(encoded);
+        return address(uint160(bytes20(ripemdHash)));
+    }
 }
