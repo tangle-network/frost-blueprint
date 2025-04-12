f209b820cbb6ac857837956eba943bd8e64e08f1
Shady Khalifa <dev+github@shadykhalifa.me>
2025-04-12 17:07:25 +0200

    refactor: update package metadata and build script paths
    - Comment out package metadata in Cargo.toml for clarity.
    - Change contract directory path in build.rs from "../contracts" to "./contracts".

--- bin/build.rs
+++ bin/build.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 use std::process;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let contract_dirs: Vec<&str> = vec!["../contracts"];
+    let contract_dirs: Vec<&str> = vec!["./contracts"];
     build::utils::soldeer_install();
     build::utils::soldeer_update();
     build::utils::build_contracts(contract_dirs);
